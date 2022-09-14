648a2785e77c53b9d9a64a953385452d22bb52b1
Leonid Shaydenko <103574243+lssleo@users.noreply.github.com>
2022-09-14 12:02:52 +0300

    Add files via upload

---
+++ deploy/99-update-front-end.js
@@ -0,0 +1,33 @@
+const { ethers, network } = require("hardhat")
+const fs = require("fs")
+
+const FRONT_END_ADDRESSES_FILE = "../game-of-chance-nextjs/constants/contractAddresses.json"
+const FRONT_END_ABI_FILE = "../game-of-chance-nextjs/constants/abi.json"
+
+module.exports = async () => {
+    if (process.env.UPDATE_FRONT_END) {
+        console.log("Writing to front end...")
+        await updateContractAddresses()
+        await updateAbi()
+        console.log("Front end written!")
+    }
+}
+
+async function updateAbi() {
+    const raffle = await ethers.getContract("Raffle")
+    fs.writeFileSync(FRONT_END_ABI_FILE, raffle.interface.format(ethers.utils.FormatTypes.json))
+}
+
+async function updateContractAddresses() {
+    const raffle = await ethers.getContract("Raffle")
+    const contractAddresses = JSON.parse(fs.readFileSync(FRONT_END_ADDRESSES_FILE, "utf8"))
+    if (network.config.chainId.toString() in contractAddresses) {
+        if (!contractAddresses[network.config.chainId.toString()].includes(raffle.address)) {
+            contractAddresses[network.config.chainId.toString()].push(raffle.address)
+        }
+    } else {
+        contractAddresses[network.config.chainId.toString()] = [raffle.address]
+    }
+    fs.writeFileSync(FRONT_END_ADDRESSES_FILE, JSON.stringify(contractAddresses))
+}
+module.exports.tags = ["all", "frontend"]
