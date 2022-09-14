fe0ce4b3d1380df1d433e3fbfc3df55560b9105a
Leonid Shaydenko <103574243+lssleo@users.noreply.github.com>
2022-09-14 12:07:50 +0300

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -10,6 +10,15 @@ const networkConfig = {
         callbackGasLimit: "500000", // 500,000 gas
         interval: "30",
     },
+    5: {
+        name: "goerli",
+        vrfCoordinatorV2: "0x2ca8e0c643bde4c2e08ab1fa0da3401adad7734d",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
+        subscriptionId: "PasteSubId",
+        callbackGasLimit: "500000", // 500,000 gas
+        interval: "90",
+    },
     31337: {
         name: "hardhat",
         entranceFee: ethers.utils.parseEther("0.01"),
